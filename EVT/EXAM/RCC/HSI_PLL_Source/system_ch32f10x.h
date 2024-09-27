fab6aba378f0537fbed3e1e1359191d065c77759
zjw939057120 <zjw939057120@outlookcom>
2024-09-27 17:23:02 +0800

    first commit

---
+++ EVT/EXAM/RCC/HSI_PLL_Source/system_ch32f10x.h
@@ -0,0 +1,38 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : system_ch32f10x.h
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2024/06/26
+ * Description        : CMSIS Cortex-M3 Device Peripheral Access Layer System Header File. 
+ *********************************************************************************
+* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+* Attention: This software (modified or not) and binary are used for 
+* microcontroller manufactured by Nanjing Qinheng Microelectronics.
+*******************************************************************************/
+#ifndef __SYSTEM_CH32F10x_H
+#define __SYSTEM_CH32F10x_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define HSI      0   //HSI
+#define HSI_1_2  1   //HSI/2
+
+//#define PLL_Source   HSI
+#define PLL_Source   HSI_1_2
+
+extern unsigned int SystemCoreClock;          /* System Clock Frequency (Core Clock) */
+
+/* CH32F10x_System_Exported_Functions */
+extern void SystemInit( void );
+extern void SystemCoreClockUpdate( void );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__CH32F10x_SYSTEM_H */
+
+
+
---
+++ EVT/EXAM/USB/USBD/MSC_U-Disk/user/USBLIB/USB-Driver/src/usb_init.c
@@ -0,0 +1,50 @@
+/********************************** (C) COPYRIGHT *******************************
+ * File Name          : usb_init.c
+ * Author             : WCH
+ * Version            : V1.0.0
+ * Date               : 2019/10/15
+ * Description        : Initialization routines & global variables
+*********************************************************************************
+* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+* Attention: This software (modified or not) and binary are used for 
+* microcontroller manufactured by Nanjing Qinheng Microelectronics.
+*******************************************************************************/ 
+#include "usb_lib.h"
+
+uint8_t	EPindex;
+DEVICE_INFO *pInformation;
+DEVICE_PROP *pProperty;
+uint16_t	SaveState ;
+uint16_t  wInterrupt_Mask;
+DEVICE_INFO	Device_Info;
+USER_STANDARD_REQUESTS  *pUser_Standard_Requests;
+
+/*********************************************************************
+ * @fn      USB_Init
+ *
+ * @brief   USB system initialization
+ *
+ * @return  none
+ */
+void USB_Init( PWR_VDD VDD_Voltage )
+{
+	if( VDD_Voltage == PWR_VDD_5V)
+	{
+		EXTEN->EXTEN_CTR |= EXTEN_USB_5V_SEL;
+	}
+	else
+	{
+		EXTEN->EXTEN_CTR &= ~EXTEN_USB_5V_SEL;
+	}
+	
+  pInformation = &Device_Info;
+  pInformation->ControlState = 2;
+  pProperty = &Device_Property;
+  pUser_Standard_Requests = &User_Standard_Requests;
+  pProperty->Init();
+}
+
+
+
+
+
